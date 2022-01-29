3d0a4ac0619af026eebbe035069ed5862c4b4bc5
MoisesDuarte <moisesduartejunior@gmail.com>
2022-01-28 22:28:42 -0300

    Implementando validação de dto de usuário

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { ReturnUserDto } from './dto/return-user.dto';
 import { UsersService } from './users.service';
@@ -9,7 +9,7 @@ export class UsersController {
 
   @Post()
   async createAdminUser(
-    @Body() createUserDto: CreateUserDto,
+    @Body(ValidationPipe) createUserDto: CreateUserDto,
   ): Promise<ReturnUserDto> {
     const user = await this.userService.createAdminUser(createUserDto);
 
